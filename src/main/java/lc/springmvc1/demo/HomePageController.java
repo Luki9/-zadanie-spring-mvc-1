b43bab7e6f12d0bb6d36033f27f5e37219b8e92f
lukasz <robson27-r@hotmail.com>
2019-03-18 17:19:10 +0100

    w trakcie

--- src/main/java/lc/springmvc1/demo/HomePageController.java
+++ src/main/java/lc/springmvc1/demo/HomePageController.java
@@ -2,19 +2,28 @@ package lc.springmvc1.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 
 @Controller
 class HomePageController {
 
+    ArrayList<Product> lab = new ArrayList<>();
 
-    ArrayList<Product>lab = new ArrayList<>();
-
-    public ArrayList<Product> getLab(){
-
+    public ArrayList<Product> getlab() {
         return lab;
     }
 
+
+    public void setLab(ArrayList<Product> lab) {
+        this.lab = lab;
+
+    }
+        @GetMapping(){
+
+        public String homepage(Model model)
+
+        }
 
 }
