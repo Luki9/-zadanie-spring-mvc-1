3bf664fd2da6f50c4aed5de8d55e6d3b23857ee1
lukasz <robson27-r@hotmail.com>
2019-03-18 14:18:37 +0100

    w trakcie

--- src/main/java/lc/springmvc1/demo/HomePageController.java
+++ src/main/java/lc/springmvc1/demo/HomePageController.java
@@ -3,6 +3,18 @@ package lc.springmvc1.demo;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+
 @Controller
 class HomePageController {
+
+
+    ArrayList<Product>lab = new ArrayList<>();
+
+    public ArrayList<Product> getLab(){
+
+        return lab;
+    }
+
+
 }
